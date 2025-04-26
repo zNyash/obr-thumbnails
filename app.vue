d52651e6d875c16b04024e1b95314ef786bce28c
Nyash <37278974+zNyash@users.noreply.github.com>
2025-04-26 02:24:21 -0300

    create get mods by bitwise value function

--- app.vue
+++ app.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-import type { ScoreInfo, ModUtils } from 'osu-classes';
-import {ScoreDecoder} from 'osu-parsers';
+import type { ScoreInfo } from 'osu-classes';
 import type { IBeatmapInfo } from './types/IBeatmapInfo';
+import type { TModKey } from './types/IModBitwise';
+import {ScoreDecoder} from 'osu-parsers';
+import _ from "lodash"
 
 getAuthToken()
 
 // Basic Objects
 const scoreInfo = ref<ScoreInfo>();
+const scoreMods = ref<TModKey[]>()
 const beatmapInfo = ref<IBeatmapInfo>()
 
 /**
@@ -25,7 +28,7 @@ async function handleFileInput(e: Event){
   scoreInfo.value = parsedScore.info
   const beatmapHashMD5 = scoreInfo.value.beatmapHashMD5
   beatmapInfo.value = await getBeatmapByHash(beatmapHashMD5)
-  
+  scoreMods.value = getMods(scoreInfo.value.rawMods)
 }
 
 
@@ -35,5 +38,14 @@ async function handleFileInput(e: Event){
   <main class="flex flex-col items-center justify-center h-[100vh]">
     <!-- Input Area -->
      <input type="file" class="bg-neutral-900 p-1 rounded-md border-neutral-700 border hover:bg-neutral-800/75 cursor-pointer" @change="handleFileInput">
+     <p>Tittle: {{ beatmapInfo?.beatmapset.title }}</p>
+     <p>Diff Name: {{ beatmapInfo?.version }}</p>
+     <p>Star Rating: {{ beatmapInfo?.difficulty_rating }}*</p>
+     <p>Player Name: {{ scoreInfo?.username }}</p>
+     <p>Play Max Combo: {{ scoreInfo?.maxCombo }}x</p>
+     <p>Map Max Combo: {{ beatmapInfo?.max_combo }}x</p>
+     <p>Play Accuracy: {{ `${scoreInfo ? _.round(scoreInfo?.accuracy * 100, 2) : ""}%`}}</p>
+     <p>Mods: {{ scoreMods }}</p>
+
   </main>
 </template>
--- utils/get-mods.ts
+++ utils/get-mods.ts
@@ -1,3 +1,19 @@
-export default () => {
-  return 'Hello Util'
+import { IModBitwise, type TModKey } from "~/types/IModBitwise"
+
+export default (rawMods: number | string): TModKey[] => {
+  const activeMods: TModKey[] = [] // Active mods array
+
+  for (const modKey in IModBitwise) {
+    const modValue = IModBitwise[modKey as TModKey]
+
+    if ((Number(rawMods) & modValue) > 0) {
+      activeMods.push(modKey as TModKey)
+    }
+  }
+
+  if (activeMods.length < 1){
+    activeMods.push("NM")
+  }
+
+  return activeMods;
 }
